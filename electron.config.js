ef7c786ba2a958355c1c59b65d8971787d02bdb7
inviqus <dunaevskiy.s@gmail.com>
2018-04-05 08:51:14 +0200

    update: electron config

--- electron.config.js
+++ electron.config.js
@@ -1,23 +1,26 @@
-const {app, BrowserWindow} = require("electron");
-const path                 = require("path");
-const url                  = require("url");
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
 
 let win;
 
 function createWindow() {
 	win = new BrowserWindow({
-		title     : "EvoDoc",
-		width     : 400,
-		height    : 600,
-		minWidth  : 400,
-		minHeight : 600
+		title: "EvoDoc",
+		width: 400,
+		height: 600,
+		minWidth: 400,
+		minHeight: 600
+		// titleBarStyle: "hiddenInset"
 	});
 
-	win.loadURL(url.format({
-		pathname : path.join(__dirname + "/index.html"),
-		protocol : "file:",
-		slashes  : true
-	}));
+	win.loadURL(
+		url.format({
+			pathname: path.join(__dirname + "/index.html"),
+			protocol: "file:",
+			slashes: true
+		})
+	);
 
 	win.webContents.openDevTools();
 
@@ -29,11 +32,9 @@ function createWindow() {
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
-	if (process.platform !== "darwin")
-		app.quit();
+	if (process.platform !== "darwin") app.quit();
 });
 
 app.on("activate", () => {
-	if (win === null)
-		createWindow();
+	if (win === null) createWindow();
 });
