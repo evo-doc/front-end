8954df8a657a021ad231286ccffb86d4a3c9b5e2
inviqus <dunaevskiy.s@gmail.com>
2018-04-16 00:25:10 +0200

    update: error pages
    - error pages have the same tpl defined as a component
    - error pages are translated

--- src/local/routes/default/error/404/index.js
+++ src/local/routes/default/error/404/index.js
@@ -5,11 +5,16 @@ const Page = require("page.class");
 class Index extends Page {
 	constructor(config, args) {
 		super(config, args);
-		this._template = require("./index.ejs");
 	}
 
 	_render(renderDone, renderFail) {
-		this._getRoot().innerHTML = this._template();
+		this._getRoot().innerHTML = this._getComponent().errors({
+			_data: {
+				title: APP.getLocalization().getPhrase("error", "404_title"),
+				message: APP.getLocalization().getPhrase("error", "404_message")
+			}
+		});
+
 		renderDone();
 	}
 }
