cf43750aefb94effe739d1a03ccc4a0a4de94cac
Sergey Dunaevskiy <dunaevskiy.s@gmail.com>
2018-04-29 17:18:10 +0200

    add: Font Awesome Icons

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,6 +2,7 @@
 
 const Application = require("application.class");
 const sass = require("./local/styles/main.scss");
+const fontawesome = require("./local/resources/libraries/fontawesome");
 
 // Initialisation, APP is a global variable
 global.APP = new Application();
