e71bc84948a066b62180e70a3935e6da30405163
Sergey Dunaevskiy <dunaevskiy.s@gmail.com>
2018-05-01 23:16:29 +0200

    add: new error types

--- src/core/modules/error.module.js
+++ src/core/modules/error.module.js
@@ -14,7 +14,7 @@
 // Export interface
 //--------------------------------------------------------------------------------------------------
 /**
- * @summary Language Error
+ * @summary Language error
  * @description Throw this exception if the requested language file is not found.
  * @param {string} localization - Localization abbr
  */
@@ -29,7 +29,7 @@ module.exports.LocalizationError = class LocalizationError extends Error {
 };
 
 /**
- * @summary Phrase Error
+ * @summary Phrase error
  * @description Throw this exception if the requested phrase does not exist.
  * @param {string} localization - Localization abbr
  * @param {string} namespace - Requested namespace
@@ -63,7 +63,7 @@ module.exports.StorageError = class StorageError extends Error {
 };
 
 /**
- * @summary Route Error
+ * @summary Route error
  * @description Throw this exception if the requested route is not found.
  * @param {number} [status=404] - Error status number
  * @param {string} path - Requested path
@@ -79,19 +79,20 @@ module.exports.RouteError = class RouteError extends Error {
 };
 
 /**
- * @summary Registration non-unique data warning
- * @description Throw this exception if user registration data are not unique.
+ * @summary Authorization data warning
+ * @description Throw this exception if server refused auth/reg request because of some data error.
  * @param {number} [status=400] - Response status
+ * @param {string} action - Sign in/sign up etc.
  * @param {string} message - Error message
  */
-module.exports.RegistrationWarning = class RegistrationWarning extends Error {
-	constructor(status = 400, message) {
+module.exports.AuthorizationWarning = class AuthorizationWarning extends Error {
+	constructor(status = 400, action, message) {
 		super();
 		this.name = this.constructor.name;
 		this.status = status;
 		this.message = message;
 		Error.captureStackTrace(this, this.constructor);
-		log.debug(`[${status}] [REGISTRATION]: non-unique ${message}`);
+		log.debug(`[${status}] [${action}]: ${message}`);
 	}
 };
 
@@ -109,6 +110,42 @@ module.exports.UnexpectedBehaviour = class UnexpectedBehaviour extends Error {
 		this.status = status;
 		this.message = message;
 		Error.captureStackTrace(this, this.constructor);
-		log.error(`[${status}] [${action}]: unexpected behavaiour - ${message}`);
+		log.error(`[${status}] [${action}]: unexpected behavaiour - ${JSON.stringify(message)}`);
+	}
+};
+
+/**
+ * @summary Invalid token
+ * @description Throw this exception if we receive 403 from the server.
+ * @param {number} [status=403] - Response status
+ * @param {string} action - User action
+ * @param {string} hash - Request hash
+ */
+module.exports.InvalidTokenError = class InvalidTokenError extends Error {
+	constructor(status = 403, action, hash) {
+		super();
+		this.name = this.constructor.name;
+		this.status = status;
+		this.message = "Invalid token";
+		Error.captureStackTrace(this, this.constructor);
+		log.warn(`[${status}] [${action}] (${hash}): ${this.message}`);
+	}
+};
+
+/**
+ * @summary Data consistency error
+ * @description Throw this exception if server return
+ * @param {number} [status=403] - Response status
+ * @param {string} action - User action
+ * @param {string} hash - Request hash
+ */
+module.exports.DataConsistencyError = class DataConsistencyError extends Error {
+	constructor(status = 403, action, hash) {
+		super();
+		this.name = this.constructor.name;
+		this.status = status;
+		this.message = "Data consistency";
+		Error.captureStackTrace(this, this.constructor);
+		log.error(`[${status}] [${action}] (${hash}): ${this.message}`);
 	}
 };
