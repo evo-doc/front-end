f41258be1dfe0b2c24dbb81defd58426ff236444
Monika Novakova <nxmonika@gmail.com>
2018-05-04 22:06:36 +0200

    update: sign in form validation

--- src/local/routes/test/authorization/sign-in/index.js
+++ src/local/routes/test/authorization/sign-in/index.js
@@ -4,6 +4,7 @@
 // TODO: User friendly response
 
 const Page = require("page.class");
+const validation = require("validation.module");
 
 class Index extends Page {
 	constructor(config, args) {
@@ -25,14 +26,25 @@ class Index extends Page {
 	}
 
 	async _sendAuthorizationForm() {
-		let username = document.getElementById("username").value;
+		let usernameOrEmail = document.getElementById("username").value;
 		let password = document.getElementById("password").value;
+		let feedback = document.getElementById("feedback");
+
+		if (!(validation.email(usernameOrEmail) && validation.username(usernameOrEmail))) {
+			feedback.innerHTML = "Invalid Username or Email";
+			return;
+		}
+
+		if (validation.pass(password)) {
+			feedback.innerHTML = "Invalid password";
+			return;
+		}
 
 		try {
 			await APP.getAuthorization().sendAuthorization(username, password);
 		} catch (e) {
 			if (e instanceof error.AuthorizationWarning) {
-				return (document.getElementById("feedback").innerHTML = e.message);
+				return (feedback.innerHTML = e.message);
 			}
 
 			if (e instanceof error.UnexpectedBehaviour) {
