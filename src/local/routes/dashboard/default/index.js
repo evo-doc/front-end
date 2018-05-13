9afa8978a10aaa147aee9eb2bd9995d59fa8bcd1
Sergey Dunaevskiy <dunaevskiy.s@gmail.com>
2018-05-13 13:12:41 +0200

    add: new routes structure; close #6

---
+++ src/local/routes/dashboard/default/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const Page = require("page.class");
+
+class Index extends Page {
+	constructor(config, args) {
+		super(config, args);
+		this._template = require("./index.ejs");
+		// this._style = require("./index.scss");
+	}
+
+	__render() {
+		this._getRoot().innerHTML = this._template();
+	}
+}
+
+module.exports = function(config = {}) {
+	return {
+		config: config,
+		page: Index
+	};
+};
---
+++ src/local/routes/dashboard/project/module/edit/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const Page = require("page.class");
+
+class Index extends Page {
+	constructor(config, args) {
+		super(config, args);
+		this._template = require("./index.ejs");
+		// this._style = require("./index.scss");
+	}
+
+	__render() {
+		this._getRoot().innerHTML = this._template({
+			_data: {
+				project: {
+					id: this._args[1]
+				},
+				module: {
+					id: this._args[2]
+				}
+			}
+		});
+	}
+}
+
+module.exports = function(config = {}) {
+	return {
+		config: config,
+		page: Index
+	};
+};
